5cf2b1f230ec0d4b5d9baa127ac9f9ddfe6af63a
Dico Diaz <dicodiaz@gmail.com>
2021-11-08 14:27:40 -0500

    Create basic structure for Missions

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,13 @@
-import { createStore } from 'redux';
-import reducer from './ducks/counter';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import missionsReducer from './ducks/missions';
 
-const store = createStore(reducer);
+const reducer = combineReducers({
+  missionsReducer,
+  // additional reducers could be added here
+});
+
+const store = createStore(reducer, applyMiddleware(logger, thunk));
 
 export default store;
