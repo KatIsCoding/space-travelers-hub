930c92491c829db0b5906b91d4f9a5634ecaba2f
Dico Diaz <dicodiaz@gmail.com>
2021-11-11 11:31:28 -0500

    Add iterative button click function

--- src/__tests__/MissionUX.test.js
+++ src/__tests__/MissionUX.test.js
@@ -6,6 +6,12 @@ import Profile from '../components/Profile';
 import testStore from '../redux/testStore';
 import getMockMissions from '../__mocks__/getMockMissions';
 
+const clickButtons = (buttons, indexes = []) => {
+  indexes.forEach((index) => {
+    fireEvent.click(buttons[index]);
+  });
+};
+
 let tree;
 describe('testing UX for missions related components', () => {
   const url = 'https://api.spacexdata.com/v3/missions';
@@ -24,27 +30,21 @@ describe('testing UX for missions related components', () => {
   test('testing MissionList UX', async () => {
     expect(await tree.findAllByTestId('mission')).toHaveLength(10);
     const buttons = await tree.findAllByText('Join Mission');
-    fireEvent.click(buttons[0]);
+    clickButtons(buttons, [0]);
     expect(await tree.findByText('Leave Mission')).toBeTruthy();
     expect(await tree.findByText('Active Member')).toBeTruthy();
-    fireEvent.click(buttons[0]);
+    clickButtons(buttons, [0]);
     expect(tree.queryByText('Leave Mission')).toBeFalsy();
     expect(tree.queryByText('Active Member')).toBeFalsy();
   });
 
   test('testing Profile UX', async () => {
     const buttons = await tree.findAllByText('Join Mission');
-    fireEvent.click(buttons[1]);
-    fireEvent.click(buttons[3]);
-    fireEvent.click(buttons[5]);
-    fireEvent.click(buttons[7]);
-    fireEvent.click(buttons[9]);
+    clickButtons(buttons, [1, 3, 5, 7, 9]);
     expect(await tree.findAllByTestId('profile-mission')).toHaveLength(5);
-    fireEvent.click(buttons[9]);
-    fireEvent.click(buttons[7]);
+    clickButtons(buttons, [7, 9]);
     expect(await tree.findAllByTestId('profile-mission')).toHaveLength(3);
-    fireEvent.click(buttons[5]);
-    fireEvent.click(buttons[3]);
+    clickButtons(buttons, [3, 5]);
     const profileMission = await tree.findByTestId('profile-mission');
     expect(await within(profileMission).findByText('Telstar')).toBeTruthy();
   });
